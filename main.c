20913a39b6487893ba5a3565b663d303443558db
miguell <miko@charape>
2025-02-26 20:02:24 +0100

    decks

--- main.c
+++ main.c
@@ -7,41 +7,53 @@
 	#include <unistd.h>
 #endif
 
-enum State{
-	MENU = 0,
-	SCOUNDREL,
-	DONSOLE,
-	DONSOLM,
-	DONSOLH,
-	SRULES,
-	DRULES,
-	QUIT
-};
+// needs to either be reformatted into one deck, called upon the start of the game or will stay like this for each new game added
 
-static enum State gamestate = MENU;
+static int ingame = 0;
+static char* scoundeck[44] = {"AC","2C","3C","4C","5C","6C","7C","8C","9C","XC","JC","QC","KC","AS","2S","3S","4S","5S","6S","7S","8S","9S","XS","JS","QS","KS","2D","3D","4D","5D","6D","7D","8D","9D","XD","2H","3H","4H","5H","6H","7H","8H","9H","XH"};
+static char* donsdeck[54] = {"AC","2C","3C","4C","5C","6C","7C","8C","9C","XC","JC","QC","KC","AS","2S","3S","4S","5S","6S","7S","8S","9S","XS","JS","QS","KS","AD","2D","3D","4D","5D","6D","7D","8D","9D","XD","JD","QD","KD","AH","2H","3H","4H","5H","6H","7H","8H","9H","XH","JH","QH","KH","jR","jB"};
+static char* regipldeck[40] = {"AC","2C","3C","4C","5C","6C","7C","8C","9C","XC","AS","2S","3S","4S","5S","6S","7S","8S","9S","XS","AD","2D","3D","4D","5D","6D","7D","8D","9D","XD","AH","2H","3H","4H","5H","6H","7H","8H","9H","XH"};
+static char* regiendeck[12] = {"JC","QC","KC","JS","QS","KS","JD","QD","KD","JH","QH","KH"};
 
 char getkey() {
 	#ifdef _WIN32
-	   char ch = _getch();
-	   return ch;
+		// windows terminal input noecho
+		char ch = _getch();
+		return ch; // maybe can just return _getch() instead?
 	#else
+		// linux terminal inout noecho
 		struct termios oldt, newt;
 		char ch;
 		tcgetattr(STDIN_FILENO, &oldt);
 		newt = oldt;
-		newt.c_lflag &= ~(ICANON | ECHO);
-		tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+		newt.c_lflag &= ~(ICANON | ECHO); // flag for disabling echo
+		tcsetattr(STDIN_FILENO, TCSANOW, &newt); // disables echo
 		ch = getchar();
-		tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+		tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // enables echo
 		return ch;
 	#endif
 }
 
 void menu(){
-	printf("Hello welcome to cardungeon!\nPlease press one of the following keys:\n1: Play Scoundrel\n2: Play Donsol (Easy)\n3: Play Donsol (Medium)\n4: Play Donsol (Hard)\nS: Scoundrel rules\nD: Donsol rules\nQ: Quit\n");
+	printf("Hello welcome to cardungeon!\nPlease press one of the following keys:\ns: Play Scoundrel\nS: Scoundrel rules\nd: Play Donsol\nD: Donsol rules\nr: Play Regicide\nR: Regicide rules\nq: Quit\n");
 	// ANSI escape codes do underline and red color
 	// 0m resets the text formatting
 	printf("\033[4;31mYou may use your keyboard now.\033[0m\n");
+}
+
+void scoundrel() {
+	ingame = 1;
+	while (ingame) {
+		
+	}
+}
+
+void donsol() {
+	printf("donsol\n");
+}
+
+void regicide() {
+	printf("regicide\n");
 }
 
 int main() {
@@ -49,44 +61,28 @@ int main() {
 	int gamerunning = 1;
 	// create an introduction
 	while(gamerunning){
-		switch (gamestate) {
-			case MENU:
-				menu();
+		menu();
 input:
-				switch (getkey()) {
-					case '1':
-						gamestate = SCOUNDREL;
-						break;
-					case '2':
-						gamestate = DONSOLE;
-						break;
-					case '4':
-						gamestate = DONSOLH;
-						break;
-					case 's':
-						gamestate = SRULES;
-						break;
-					case 'd':
-						gamestate = DRULES;
-						break;
-					case 'q':
-						gamerunning = 0;
-						break;
-					case '5':
-						gamestate = SRULES;
-						break;
-					case 'D':
-						gamestate = DRULES;
-						break;
-					case 'Q':
-						gamerunning = 0;
-						break;
-					default:
-						goto input;
-				}
+		switch (getkey()) {
+			case 's':
+				break;
+			case '5':
+				break;
+			case 'd':
+				donsol();
+				break;
+			case 'D':
+				break;
+			case 'r':
+				regicide();
+				break;
+			case 'R':
+				break;
+			case 'q':
+				gamerunning = 0;
 				break;
 			default:
-				break;
+				goto input;
 		}
 	}
 	printf("byebye");
